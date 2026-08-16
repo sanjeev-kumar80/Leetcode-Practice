377c5ad92655975796a4f54cda7a35bc7fcff469
Sanjeev Kumar <sanjeev20565@gmail.com>
2026-08-17 01:25:45 +0530

    Time: 1 ms (95.87%), Space: 47.1 MB (7.83%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,67 +1,31 @@
 class Solution {
-    public void bfs(TreeNode root, List<List<Integer>> ans) {
 
+    public void bfs(TreeNode root, List<List<Integer>> ll) {
         Queue<TreeNode> q = new LinkedList<>();
+        if (root == null)
+            return;
+        q.add(root);
 
-        if (root != null)
-            q.add(root);
-
-        while (q.size() > 0) {
-
-            List<Integer> ll = new ArrayList<>();
+        while (!q.isEmpty()) {
             int size = q.size();
+            List<Integer> l = new ArrayList<>();
 
             for (int i = 0; i < size; i++) {
-
-                TreeNode temp = q.remove();
-
-                ll.add(temp.val);
+                TreeNode temp = q.poll();
+                l.add(temp.val);
 
                 if (temp.left != null)
                     q.add(temp.left);
-
                 if (temp.right != null)
                     q.add(temp.right);
             }
-
-            ans.add(ll);
+            ll.add(l);
         }
     }
 
-
-    public int height(TreeNode root){
-        if(root==null || (root.left==null && root.right==null)) return 0;
-
-        return 1+Math.max(height(root.left),height(root.right));
-    }
-
-    public void helper(TreeNode root,List<Integer> ll,int i){
-        if(root==null) return;
-
-        if(i==1) {
-            ll.add(root.val);
-            return;
-        }
-        helper(root.left,ll,i-1);
-        helper(root.right,ll,i-1);
-    }
     public List<List<Integer>> levelOrder(TreeNode root) {
-
-        List<List<Integer>> ans=new ArrayList();
-        bfs(root,ans);
-        // if(root==null) return ans;
-
-        // for(int i=1;i<=height(root)+1;i++){
-        //     List<Integer> ll = new ArrayList<>();
-        //     helper(root,ll,i);
-        //     ans.add(ll);
-
-        // }
-        return ans;
+        List<List<Integer>> ll = new ArrayList<>();
+        bfs(root, ll);
+        return ll;
     }
 }
-
-
-
-
-
